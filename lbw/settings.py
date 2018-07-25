5682e97b91a4428e8732d139b2adfaa4ba2ee61e
Liam Bedford <lbedford@lbedford.org>
2018-07-25 17:32:08 +0200

    Add a first version of the docker configs

---
+++ lbw/settings.py
@@ -0,0 +1,228 @@
+"""
+Django settings for lbw project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '9e^_(#bpz*^kp2+2@tev(7%e57*!#!(y0=f7u3-f+gt5lc5-ab'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Liam Bedford', 'lbedford@lbedford.org'),
+)
+
+MANAGERS = ADMINS
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['reg.linuxbierwanderung.com']
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Running Health Checks
+    'health_check',
+    'health_check.db',
+
+    # dependencies
+    'crispy_forms',
+
+    # actual LBW apps
+    'accounts',
+    'registration',
+)
+
+MIDDLEWARE  = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'lbw.urls'
+
+WSGI_APPLICATION = 'lbw.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lbw',                      # Or path to database file if using sqlite3.
+        'CONN_MAX_AGE': 3600,
+        'USER': os.getenv('MARIADB_USER'),
+        'PASSWORD': os.getenv('MARIADB_PASSWORD'),
+        'HOST': os.getenv('MARIADB_HOST'),                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': os.getenv('MARIADB_PORT', 3306),                      # Set to empty string for default.
+        'OPTIONS': { 'charset': 'utf8mb4', 'sql_mode': 'TRADITIONAL',
+            'init_command': 'SET '
+                'default_storage_engine=INNODB,'
+                'character_set_connection=utf8mb4,'
+                'collation_connection=utf8mb4_bin;'
+                'SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',
+ },
+    }
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/var/www/content/static'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# old settings ===================
+
+SITE_ID = 1
+
+
+# Media (user uploads)
+#
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = '/var/www/content/media'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Templates
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+TEMPLATES = [
+    {   
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.csrf',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', (
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',)), ],
+            'string_if_invalid': 'Undefined variable used, please file a bug at <a href="http://github.com/lbedford/lbwreg.django">github</a>',
+        },
+    },
+]
+
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+
+# EMail settings
+DEFAULT_FROM_EMAIL = 'lbw@draiocht.net'
+SERVER_EMAIL = 'django@lbwreg.draiocht.net'
+
+LBW_TO_EMAIL = ['linuxbierwanderung@googlegroups.com']
+LBW_FROM_EMAIL = 'lbw@draiocht.net'
+
+DATE_FORMAT = 'Y-m-d'
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# deployment settings for https
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
