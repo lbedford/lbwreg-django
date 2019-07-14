c3d3a0f7ea71d12201d424abd002b2c5e50d9acf
Liam Bedford <lbedford@lbedford.org>
2019-07-14 17:05:04 +0200

    listen on all interfaces

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ cd /app
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --run-syncdb
 
-exec gunicorn -w 4 --forwarded-allow-ips="*" lbw.wsgi
+exec gunicorn -b :8000 -w 4 --forwarded-allow-ips="*" lbw.wsgi
