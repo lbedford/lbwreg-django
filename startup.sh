724c6b0854f753bfdaf9e1ffdb41702025282c34
Liam Bedford <lbedford@lbedford.org>
2019-07-15 07:39:42 +0200

    allow workers to be overridden

--- startup.sh
+++ startup.sh
@@ -4,4 +4,5 @@ cd /app
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --run-syncdb
 
-exec gunicorn -b :8000 -w 4 --forwarded-allow-ips="*" --capture-output lbw.wsgi
+# WORKERS is set in deployment
+exec gunicorn -b :8000 -w ${WORKERS:-4} --forwarded-allow-ips="*" --capture-output lbw.wsgi
